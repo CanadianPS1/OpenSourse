e1a9679a3caa220d243f8170437f17db62a9a63e
Lia <sethroeback1@gmail.com>
2025-07-03 11:52:09 -0600

    made the unit tests

--- calculator/src/test/java/com/csc180/CalculatorTest.java
+++ calculator/src/test/java/com/csc180/CalculatorTest.java
@@ -1,12 +1,26 @@
 package com.csc180;
  
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
- 
-import static org.junit.jupiter.api.Assertions.*;
  
 public class CalculatorTest {
  
     Calculator calc = new Calculator();
- 
+    @Test
+    void testAdd(){
+        assertEquals(5.0,calc.add(2.0,3.0));
+    }
+    @Test
+    void testSub(){
+        assertEquals(1.0,calc.sub(2.0,1.0));
+    }
+    @Test
+    void testMalt(){
+        assertEquals(10.0,calc.malt(5.0,2.0));
+    }
+    @Test
+    void testDev(){
+        assertEquals(5.0,calc.dev(10.0,2.0));
+    }
    
 }
