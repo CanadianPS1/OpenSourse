82b5bb0478b3be689a1ed21dadfca3f8f2f2f437
Lia <sethroeback1@gmail.com>
2025-07-07 11:44:57 -0600

    did stuff

---
+++ ConsoleIO.java
@@ -0,0 +1,65 @@
+package lib;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ConsoleIO {
+
+	private static BufferedReader buffy = new BufferedReader(new InputStreamReader(System.in));
+
+	/**
+	 * Generates a prompt that allows the user to enter any response and returns the String.
+	 * This method never accepts null inputs. This method will accept
+	 * empty or whitespace-only inputs when allowBlank is true.
+	 * @param prompt - the prompt to be displayed to the user.
+	 * @param allowBlank - determines whether empty/whitespace-only inputs should be accepted
+	 * @return the input from the user as a String
+	 */
+	public static String promptForString(String prompt, boolean allowBlank){
+		//TODO Implement this method with the proper logic
+		throw new UnsupportedOperationException("The promptForString method is not yet implemented.");
+	}
+	
+	/**
+	 * Generates a prompt that expects a numeric input representing an int value.
+	 * This method loops until valid input is given.
+	 * @param prompt - the prompt to be displayed to the user
+	 * @param min - the inclusive minimum boundary
+	 * @param max - the inclusive maximum boundary
+	 * @return the int value
+	 */
+	public static int promptForInt(String prompt, int min, int max){
+		//TODO Implement this method with the proper logic
+		throw new UnsupportedOperationException("The promptForInt method is not yet implemented.");
+	}
+	
+	/**
+	 * Generates a prompt that expects the user to enter one of two responses that will equate
+	 * to a boolean value. The trueString represents the case insensitive response that will equate to true. 
+	 * The falseString acts similarly, but for a false boolean value.
+	 * 		Example: Assume this method is called with a trueString argument of "yes" and a falseString argument of
+	 * 		"no". If the user enters "YES", the method returns true. If the user enters "no", the method returns false.
+	 * 		All other inputs are considered invalid, the user will be informed, and the prompt will repeat.
+	 * @param prompt - the prompt to be displayed to the user
+	 * @param trueString - the case insensitive value that will evaluate to true
+	 * @param falseString - the case insensitive value that will evaluate to false
+	 * @return the boolean value
+	 */
+	public static boolean promptForBoolean(String prompt, String trueString, String falseString){
+		//TODO Implement this method with the proper logic
+		throw new UnsupportedOperationException("The promptForBoolean method is not yet implemented.");
+	}
+
+	/**
+	 * Generates a console-based menu using the Strings in options as the menu items.
+	 * Reserves the number 0 for the "quit" option when withQuit is true.
+	 * @param options - Strings representing the menu options
+	 * @param withQuit - adds option 0 for "quit" when true
+	 * @return the int of the selection made by the user
+	 */
+	public static int promptForMenuSelection(String[] options, boolean withQuit){
+		//TODO Implement this method with the proper logic
+		throw new UnsupportedOperationException("The promptForMenuSelection method is not yet implemented.");
+	}
+
+}
---
+++ FileO.java
@@ -0,0 +1,5 @@
+public class FileO {
+    public static void main(String [] args){
+        ConsoleIO.promptForString("HI hi", false);
+    }
+}
