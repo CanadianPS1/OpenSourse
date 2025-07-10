939a688514e7f9011ec6a858061a96cae2ab0a22
Lia <122105453+CanadianPS1@users.noreply.github.com>
2025-07-10 14:06:44 -0600

    yes

--- number_guessing_game/src/main/java/csc180/roeback/lia/App.java
+++ number_guessing_game/src/main/java/csc180/roeback/lia/App.java
@@ -1,8 +1,7 @@
 package csc180.roeback.lia;
 import java.util.Random;
-import java.io.IOException;
 import java.util.ArrayList;
-import ConsoleIO.jar;
+import ConsoleIO.ConsoleIO;
 public class App{
     public static void main( String[] args ){
         promptForDiffuculty();
@@ -10,20 +9,16 @@ public class App{
     //prompts for the diffeculty and starts the game
     public static void promptForDiffuculty(){
         while(true){
-            try{
-                String difficulty = ConsoleIO.promptForString("~SELCT DIFFUCULTY~ \n EASY \n MEDIUM \n HARD", false);
-                difficulty = difficulty.toLowerCase();
-                if(difficulty.equals("easy")){
-                    guessing(1,10,5);
-                }else if(difficulty.equals("medium")){
-                    guessing(1,50,5);
-                }else if(difficulty.equals("hard")){
-                    guessing(1,100,5);
-                }else{
-                    continue;
-                }
-            }catch(IOException e){
-                System.out.println("ERROR: FAILED TO ENTER A CORRECT STRING");
+            String difficulty = ConsoleIO.promptForString("~SELCT DIFFUCULTY~ \n EASY \n MEDIUM \n HARD", false);
+            difficulty = difficulty.toLowerCase();
+            if(difficulty.equals("easy")){
+                guessing(1,10,5);
+            }else if(difficulty.equals("medium")){
+                guessing(1,50,5);
+            }else if(difficulty.equals("hard")){
+                guessing(1,100,5);
+            }else{
+                continue;
             }
         }
     }
@@ -31,12 +26,13 @@ public class App{
     public static void guessing(int min, int max, int maxTrys){
         Random rand = new Random();
         int trys = 0;
+        int guess = 0;
         int randomNum = rand.nextInt(min,max + 1);
         ArrayList<Integer> priorGuesses = new ArrayList<>();
         while(true){
             try{
                 while(trys <= maxTrys){
-                    int guess = ConsoleIO.promptForInt("Guess a Number between " + min + " and " + max + ": ", min, max);
+                    guess = ConsoleIO.promptForInt("Guess a Number between " + min + " and " + max + ": ", min, max);
                     if(!priorGuesses.contains(guess)){
                         if(guess == randomNum){
                             System.out.println("YOU GUESSED THE NUMBER!!! \n YOU WON IN " + trys + " GUESSES");
@@ -46,24 +42,26 @@ public class App{
                         }else{
                             System.out.println(guess + " is GRATER THAN the random num");
                         }
+                        System.out.println("GUESSES: " + trys);
+                        priorGuesses.add(guess);
                         trys++;
                     }else{
                         System.out.println("You Entered " + guess + " Twice, your guess has been refunded");
                     }
                 }
-                System.out.println("you FAILED \n the number was " + randomNum + "\n Would you like to play again? \nYES\nNO");
+                if(trys > maxTrys){
+                    System.out.println("you FAILED \n the number was " + randomNum + "\n Would you like to play again? \nYES\nNO");
+                }else{
+                    System.out.println(" Would you like to play again? \\n" + "YES\\n" + "NO");
+                }
                 while(true){
-                    try{
-                        String playAgain = ConsoleIO.promptForString("", false);
-                        playAgain = playAgain.toLowerCase();
-                        if(playAgain.equals("yes")){
-                            promptForDiffuculty();
-                        }else if(playAgain.equals("no")){
-                            System.out.println("Have a nice day :)");
-                            System.exit(0);
-                        }
-                    }catch(IOException e){
-                        System.out.println("ERROR: FAILED TO ENTER A CORRECT STRING");
+                    String playAgain = ConsoleIO.promptForString("", false);
+                    playAgain = playAgain.toLowerCase();
+                    if(playAgain.equals("yes")){
+                        promptForDiffuculty();
+                    }else if(playAgain.equals("no")){
+                        System.out.println("Have a nice day :)");
+                        System.exit(0);
                     }
                 }
             }catch(NumberFormatException e){
