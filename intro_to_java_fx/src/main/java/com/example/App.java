99dda98f5495e41f0f23f4cad5102fb8f5215668
Lia <sethroeback1@gmail.com>
2025-08-06 11:54:31 -0600

    changed some var locations

--- intro_to_java_fx/src/main/java/com/example/App.java
+++ intro_to_java_fx/src/main/java/com/example/App.java
@@ -2,20 +2,28 @@ package com.example;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class App extends Application{
+    private EventHandler<ActionEvent> onClick;
     private int clickAmount = 0;
-    public static void main(String [] args){
-        launch();
-    }
+    private Button button;
+    private Scene scene;
     @Override
     public void start(@SuppressWarnings("exports") Stage stage){
-        var button = new Button("Click Me!!!");
-        var scene = new Scene(new StackPane(button), 640, 480);
-        EventHandler<ActionEvent> onClick = (ActionEvent e) -> {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
+        button = new Button("Click Me!!!");
+        scene = new Scene(new StackPane(button), 640, 480);
+        onClick = (ActionEvent e) -> {
             addToCount();
             button.setText("Clicks: " + clickAmount + " WOW");
             System.out.println(clickAmount);
@@ -24,7 +32,11 @@ public class App extends Application{
         stage.setScene(scene);
         stage.show();
     }
+    public static void main(String [] args){
+        launch();
+    }
     public void addToCount(){
         clickAmount++;
     }
+    
 }
