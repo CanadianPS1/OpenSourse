7df5fd5aa4bd46071446ba1350171d28555b0e20
Lia <sethroeback1@gmail.com>
2025-08-06 11:35:19 -0600

    finished the click button app

--- intro_to_java_fx/src/main/java/com/example/App.java
+++ intro_to_java_fx/src/main/java/com/example/App.java
@@ -4,30 +4,27 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class App extends Application{
+    private int clickAmount = 0;
     public static void main(String [] args){
         launch();
     }
     @Override
     public void start(@SuppressWarnings("exports") Stage stage){
-        int clickAmount = 0;
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
-        var label = new Label("You have Clicked the Button " + clickAmount + " times WOW");
-        var scene = new Scene(new StackPane(label), 640, 480);
         var button = new Button("Click Me!!!");
-        EventHandler<ActionEvent> onClick = new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent e){
-                clickAmount += 1;
-                label.setText("You have Clicked the Button " + clickAmount + " times WOW");
-            }
+        var scene = new Scene(new StackPane(button), 640, 480);
+        EventHandler<ActionEvent> onClick = (ActionEvent e) -> {
+            addToCount();
+            button.setText("Clicks: " + clickAmount + " WOW");
+            System.out.println(clickAmount);
         };
         button.setOnAction(onClick);
         stage.setScene(scene);
         stage.show();
+    }
+    public void addToCount(){
+        clickAmount++;
     }
 }
