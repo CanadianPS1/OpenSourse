70315469c344a5e1b729491d4946d87a38b7c9ac
Lia <sethroeback1@gmail.com>
2025-08-07 11:28:06 -0600

    moved some code around

--- intro_to_java_fx/src/main/java/com/example/App.java
+++ intro_to_java_fx/src/main/java/com/example/App.java
@@ -7,27 +7,27 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 public class App extends Application{
     private EventHandler<ActionEvent> onClick;
     private int clickAmount = 0;
     private Button button;
     private Scene scene;
+    private GridPane grid;
     @Override
     public void start(@SuppressWarnings("exports") Stage stage){
-        GridPane grid = new GridPane();
+        grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(25,25,25,25));
+        scene = new Scene(grid, 640, 480);
         button = new Button("Click Me!!!");
-        scene = new Scene(new StackPane(button), 640, 480);
         onClick = (ActionEvent e) -> {
             addToCount();
             button.setText("Clicks: " + clickAmount + " WOW");
-            System.out.println(clickAmount);
         };
+        grid.add(button,1,1);
         button.setOnAction(onClick);
         stage.setScene(scene);
         stage.show();
