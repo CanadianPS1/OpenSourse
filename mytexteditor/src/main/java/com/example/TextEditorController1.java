48c2d4d9a1ff2e06b027b8948522e39eddb0ae0e
Lia <sethroeback1@gmail.com>
2025-08-11 11:42:50 -0600

    created the text editor

---
+++ mytexteditor/src/main/java/com/example/TextEditorController1.java
@@ -0,0 +1,31 @@
+package com.example; 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+ 
+public class TextEditorController1 {
+ 
+    @FXML
+    private TextArea textArea;
+ 
+    @FXML
+    private Label statusLabel;
+ 
+    @FXML
+    private void handleOpen() {
+       
+        System.out.println("Open clicked");
+    }
+ 
+    @FXML
+    private void handleSave() {
+       
+        System.out.println("Save clicked");
+    }
+ 
+    @FXML
+    private void handleAbout() {
+       
+        System.out.println("About clicked");
+    }
+}
