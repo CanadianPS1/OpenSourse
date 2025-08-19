5f7d19b8e23a55a5e6a2ada2315c7c3b9c9a332f
Lia <sethroeback1@gmail.com>
2025-08-19 17:23:02 -0600

    worked on final

--- hangman/src/main/java/com/example/App.java
+++ hangman/src/main/java/com/example/App.java
@@ -1,10 +1,9 @@
 package com.example;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
@@ -13,65 +12,51 @@ public class App extends Application{
     private EventHandler<ActionEvent> onClick;
     private GridPane grid;
     private Scene scene;
-    private Label topBar1;
-    private Label topBar2;
-    private Label topBar3;
-    private Label botBar1;
-    private Label botBar2;
-    private Label botBar3;
-    private Label rightBar1;
-    private Label rightBar2;
-    private Label rightBar3;
     private final int height = 8;
     private final int width = 8;
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final String barColor = "#0e0d0cff";
     @Override
-    public void start(Stage stage){
+    public void start(@SuppressWarnings("exports") Stage stage){
         //sets up my grid
         grid = new GridPane();
         //~creates the map~
         for(int i = 0; i < height; i++){
             for(int j = 0; j < width; j++){
-                System.out.println(j + " : " + i);
-                grid.add(createSolidColorLabel("-fx-background-color: #d7d18fff;", 640, 480),width,height);
+                grid.add(createSolidColorLabel("-fx-background-color: #f5e6b5ff;", 640,480),j,i);
             }
         }
-        //puts a boarder around the play area
-        //creates the left border
-        // grid.add(createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480),0,1);
-        // grid.add(createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480),0,2);
-        // grid.add(createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480),0,3);
-        // //creates the top boarder
-        // topBar1 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 680, 20);
-        // grid.add(topBar1,0,1);
-        // GridPane.setValignment(topBar1, VPos.TOP);
-        // topBar2 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 680, 20);
-        // grid.add(topBar2,1,1);
-        // GridPane.setValignment(topBar2, VPos.TOP);
-        // topBar3 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 680, 20);
-        // grid.add(topBar3,2,1);
-        // GridPane.setValignment(topBar3, VPos.TOP);
-        // //creates the bottom boarder
-        // botBar1 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 680, 20);
-        // grid.add(botBar1,0,3);
-        // GridPane.setValignment(botBar1, VPos.BOTTOM);
-        // botBar2 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 680, 20);
-        // grid.add(botBar2,1,3);
-        // GridPane.setValignment(botBar2, VPos.BOTTOM);
-        // botBar3 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 680, 20);
-        // grid.add(botBar3,2,3);
-        // GridPane.setValignment(botBar3, VPos.BOTTOM);
-        // grid.add(createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480),0,1);
-        // //creates the right boarder
-        // rightBar1 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480);
-        // grid.add(rightBar1,2,1);
-        // GridPane.setHalignment(rightBar1, HPos.RIGHT);
-        // rightBar2 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480);
-        // grid.add(rightBar2,2,2);
-        // GridPane.setHalignment(rightBar2, HPos.RIGHT);
-        // rightBar3 = createSolidColorLabel("-fx-background-color: #1c2b1cff", 20, 480);
-        // grid.add(rightBar3,2,3);
-        // GridPane.setHalignment(rightBar3, HPos.RIGHT);
+        //top
+        for(int i = 0; i < width; i++){
+            Label bar;
+            bar = createSolidColorLabel("-fx-background-color: " + barColor, 680,20);
+            grid.add(bar,i,height-(height-1));
+            GridPane.setValignment(bar, VPos.TOP);
+        }
+        //right
+        for(int i = 0; i < width; i++){
+            if(i > (height - (height))){
+                Label bar;
+                bar = createSolidColorLabel("-fx-background-color: " + barColor, 20,480);
+                grid.add(bar,width-1,i);
+                GridPane.setHalignment(bar, HPos.RIGHT);
+            }
+        }
+        //left
+        for(int i = 0; i < height; i++){
+            if(i > (height - (height))){
+                Label bar;
+                bar = createSolidColorLabel("-fx-background-color: " + barColor, 20,480);
+                grid.add(bar,0,i);
+                GridPane.setHalignment(bar, HPos.LEFT);
+            }
+        }
+        //bottom
+        for(int i = 0; i < width; i++){
+            Label bar;
+            bar = createSolidColorLabel("-fx-background-color: " + barColor, 680,20);
+            grid.add(bar,i,height-1);
+            GridPane.setValignment(bar, VPos.BOTTOM);
+        }
         scene = new Scene(grid, 640, 480);
         stage.setScene(scene);
         stage.show();
